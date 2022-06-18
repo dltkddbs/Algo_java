f221707a49e7b591194fc7a3544599b382ca1d0a
zlflshdh84 <zlflshdh84@gmail.com>
2022-06-18 23:57:32 +0900

    Feat: Programmers Lv1_String_Sort solved

---
+++ PROGRAMMERS/Lv1_String_Sort.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Lv1_String_Sort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.charAt(n) == o2.charAt(n)) {
+                    return o1.compareTo(o2);
+                }
+                return o1.charAt(n) - o2.charAt(n);
+            }
+        });
+
+        return strings;
+    }
+}
